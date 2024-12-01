69b995c90fdc8f4de335a21f08692028291a888f
xiaolv <sunmozong@gmail.com>
2024-12-01 20:08:10 +0800

    添加登录状态管理和路由守卫，确保未登录用户无法访问主页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,6 @@
+import { localCache } from '@/utils/cache'
 import { createRouter,createWebHashHistory} from 'vue-router'
+import {LOGIN_TOKEN} from '../global/constanants'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -23,6 +25,16 @@ const router = createRouter({
 })
 
 //导航守卫
+//参数:to(跳转的位置),from(从哪里跳转)
+//返回值:返回值决定导航的路径(不返回或者返回undefined,则不跳转)
+router.beforeEach((to)=>{
+  //只有登录成功(token),才能访问main页面
+  const token = localCache.getCache(LOGIN_TOKEN)
+
+  if(to.path === '/main' && !token){
+    return '/login'
+  }
+})
 
 export default router
 
