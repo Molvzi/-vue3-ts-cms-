d0f66382637aaeaf013ff709bc2b9d0b477c8132
xiaolv <sunmozong@gmail.com>
2024-12-01 20:15:43 +0800

    添加退出登录功能，删除登录令牌并跳转至登录页面

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,24 +1,31 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="main">
-    <h2>main:{{ counterStore.counter }}-{{ counterStore.doubleCounter }}</h2>
-    <button @click="changeCounter">修改counter</button>
+    <h2>main</h2>
+    <button @click="handleExitClick">退出登录</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import useCounterStore from '@/store/counter';
+import { LOGIN_TOKEN } from '@/global/constanants'
+import router from '@/router';
+import { localCache } from '@/utils/cache'
 
-const counterStore = useCounterStore();
-
-function changeCounter(){
-  counterStore.changeCounterAction(999);
+function handleExitClick() {
+  //1.删除token
+  localCache.removeCache(LOGIN_TOKEN)
+  //2.跳转到登录页面
+  router.push('/login')
 }
-
 </script>
 
 <style lang="less" scoped>
 .main {
   color: #000;
+}
+button {
+  margin-top: 20px;
+  color: aquamarine;
+  background-color: #000;
 }
 </style>
