c5a702d9d535d0c88fc61750db8f94d4bef3c980
xiaolv <sunmozong@gmail.com>
2024-12-03 16:25:34 +0800

    优化主头部组件，添加菜单图标和个人信息显示，调整样式以提升布局

--- src/components/main-header/main-header.vue
+++ src/components/main-header/main-header.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="main-header">
-    <h2>main-header</h2>
+    <div class="menu-icon">
+      <el-icon size="28px">
+        <Fold />
+      </el-icon>
+    </div>
+    <div class="content">
+      <div class="breadcrumb">面包屑</div>
+      <div class="info">个人信息</div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="less" scoped>
 .main-header {
-  color: #000;
+  display: flex;
+  align-items: center;
+  flex: 1;
+  height: 100%;
+
+  .menu-icon {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 18px;
+  }
 }
 </style>
