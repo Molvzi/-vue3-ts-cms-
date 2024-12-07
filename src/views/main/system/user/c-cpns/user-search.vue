df990a6cfa9110091f0e568d1d7a3f27898daaad
xiaolv <sunmozong@gmail.com>
2024-12-07 11:37:46 +0800

    添加用户搜索组件，优化表单布局，增加状态和时间范围查询功能

--- src/views/main/system/user/c-cpns/user-search.vue
+++ src/views/main/system/user/c-cpns/user-search.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="search">
-    <h2>search</h2>
+    <el-form label-width="80px" size="large">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item label="用户名:">
+            <el-input placeholder="请输入查询的用户名" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="真实姓名:">
+            <el-input placeholder="请输入查询的用户名" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="电话号码:">
+            <el-input placeholder="请输入查询的用户名" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-form-item label="状态:">
+            <el-select placeholder="请选择查询状态">
+              <el-option label="启用" :value="1" />
+              <el-option label="禁用" :value="0" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="创建时间:">
+            <el-date-picker
+              type="daterange"
+              range-separator="-"
+              start-placeholder="开始时间"
+              end-placeholder="结束时间"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
   </div>
 </template>
 
@@ -10,5 +48,10 @@
 .search {
   background: white;
   padding: 20px;
+
+  .el-form-item {
+    padding: 20px 30px;
+    margin-bottom: 0;
+  }
 }
 </style>
