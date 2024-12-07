e98b133ef2a7722518ff876c21bdf34a2d90599e
xiaolv <sunmozong@gmail.com>
2024-12-07 22:55:05 +0800

    添加用户列表表格组件，使用 ElTable 显示用户信息，优化用户内容组件样式

--- src/views/main/system/user/c-cpns/user-content.vue
+++ src/views/main/system/user/c-cpns/user-content.vue
@@ -6,11 +6,50 @@
         <el-button type="primary">新建用户</el-button>
       </div>
       <div class="table">
-        <ul>
-          <li v-for="item in usersList" :key="item.id">
-            {{ item.name }}
-          </li>
-        </ul>
+        <el-table :data="usersList" border style="width: 100%">
+          <el-table-column align="center" type="selection" width="50" />
+          <el-table-column
+            align="center"
+            type="index"
+            label="序号"
+            width="60"
+          />
+          <el-table-column
+            align="center"
+            prop="name"
+            label="用户名"
+            width="120"
+          />
+          <el-table-column
+            align="center"
+            prop="realname"
+            label="真实姓名"
+            width="120"
+          />
+          <el-table-column
+            align="center"
+            prop="cellphone"
+            label="电话号码"
+            width="150"
+          />
+          <el-table-column
+            align="center"
+            prop="enable"
+            label="状态"
+            width="80"
+          />
+          <el-table-column align="center" prop="createAt" label="创建时间" />
+          <el-table-column align="center" prop="updateAt" label="更新时间" />
+
+          <el-table-column align="center" label="操作" width="150">
+            <el-button type="primary" size="small" icon="Edit" text
+              >编辑</el-button
+            >
+            <el-button type="danger" size="small" icon="Delete" text
+              >删除</el-button
+            >
+          </el-table-column>
+        </el-table>
       </div>
       <div class="pagination">分页</div>
     </div>
@@ -44,6 +83,17 @@ const { usersList } = storeToRefs(systemStore)
     .title {
       font-size: 22px;
     }
+  }
+}
+
+.table {
+  .el-table_cell {
+    padding: 12px 0;
+  }
+
+  .el-button {
+    margin-left: 0;
+    padding: 5px 8px;
   }
 }
 </style>
