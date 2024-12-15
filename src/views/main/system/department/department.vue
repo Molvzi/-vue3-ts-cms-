cfbd41e13d591796813449c9289fb3bbbfaf08a8
xiaolv <sunmozong@gmail.com>
2024-12-15 18:16:50 +0800

    添加部门搜索功能，创建搜索表单组件并在部门页面中引入

--- src/views/main/system/department/department.vue
+++ src/views/main/system/department/department.vue
@@ -1,16 +1,12 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="department">
-    <h2>department</h2>
+    <page-search/>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import pageSearch from './c-cpns/page-search.vue'
 </script>
 
-<style lang="less" scoped>
-.department {
-  color: #000;
-}
-</style>
+<style lang="less" scoped></style>
